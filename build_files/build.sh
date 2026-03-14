58f2bad2fbeb0692a15aae390d0f5ce68ccd994e
tilodavis <tilodavis@gmx.de>
2026-03-14 14:29:00 +0000

    Update build.sh
    added cosmic epoch repo & install cosmic desktop

--- build_files/build.sh
+++ build_files/build.sh
@@ -12,6 +12,9 @@ set -ouex pipefail
 # this installs a package from fedora repos
 dnf5 install -y tmux 
 
+dnf5 -y copr enable ryanabx/cosmic-epoch
+dnf5 -y install cosmic-desktop
+
 # Use a COPR Example:
 #
 # dnf5 -y copr enable ublue-os/staging
